e697ed92dffe27b914c18a2511e3b95dcb02ac4f
Deepak <deepak@skor.co>
2026-03-29 16:25:04 +0530

    client side overlapping problem fixed

--- client/server.go
+++ client/server.go
@@ -49,7 +49,6 @@ func startTunneling(protocol, port string) {
 	fmt.Println("🌐 Public URL → http://" + string(publicUrl))
 
 	//make request on local server
-	localServer := "http://localhost:" + port
 
 	for {
 		buffer := make([]byte, 4096)
@@ -68,32 +67,40 @@ func startTunneling(protocol, port string) {
 			fmt.Println("Error unmarshalling request:", err)
 			return
 		}
+
 		fmt.Println("tunnelRequest:", tunnelRequest)
 		fmt.Println("body:", string(tunnelRequest.Body))
-		reqUrl, err := http.NewRequest(tunnelRequest.Method, localServer+tunnelRequest.Path, bytes.NewBuffer(tunnelRequest.Body))
-		reqUrl.Header = tunnelRequest.Headers
-		client := &http.Client{}
-		resp, err := client.Do(reqUrl)
-		if err != nil {
-			fmt.Println("Error making request to local server:", err)
-			return
-		}
-		defer resp.Body.Close()
-		respObj := TunnelResponse{
-			Status:  resp.StatusCode,
-			Headers: resp.Header,
-		}
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println("Error reading response from local server:", err)
-			return
-		}
-		respObj.Body = body
-		fmt.Println("Response:", string(body))
 
-		responseData, err := json.Marshal(respObj)
-		conn.Write(responseData)
+		//wait here to get data
+		go handleRequest(conn, tunnelRequest, port)
 	}
+}
+
+func handleRequest(conn net.Conn, request TunnelRequest, port string) {
+	client := &http.Client{}
+	localServer := "http://localhost:" + port
+	reqUrl, err := http.NewRequest(request.Method, localServer+request.Path, bytes.NewBuffer(request.Body))
+	reqUrl.Header = request.Headers
+	resp, err := client.Do(reqUrl)
+	if err != nil {
+		fmt.Println("Error making request to local server:", err)
+		return
+	}
+	defer resp.Body.Close()
+	respObj := TunnelResponse{
+		Status:  resp.StatusCode,
+		Headers: resp.Header,
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response from local server:", err)
+		return
+	}
+	respObj.Body = body
+	fmt.Println("Response:", string(body))
+
+	responseData, err := json.Marshal(respObj)
+	conn.Write(responseData)
 }
 
 type TunnelRequest struct {
